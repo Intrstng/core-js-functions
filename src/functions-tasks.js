70a751e200b3c55993027ac9b0db31a0f7c55b54
Intrstng <intrstg@gmail.com>
2025-04-12 23:45:14 +0300

    feat: complete task 6

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -137,8 +137,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function innerFn() {
+    let resultError;
+    for (let i = 1; i <= attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        resultError = error;
+      }
+    }
+    throw new Error(`Error occurred: ${resultError}`);
+  };
 }
 
 /**
