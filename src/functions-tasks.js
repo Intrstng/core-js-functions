715a3692c43f60186abe553cce757f3ff3def23e
Intrstng <intrstg@gmail.com>
2025-04-13 21:17:47 +0300

    feat: complete task 8

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -203,8 +203,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function innerFn(...args2) {
+    return [...args1, ...args2].join('');
+  };
 }
 
 /**
